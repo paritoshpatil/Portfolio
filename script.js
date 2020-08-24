a720895ac0f47b64ecbaab480548b5014f785b7e
paritoshpatil <paritosh.patil@tomtom.com>
2020-08-25 01:25:07 +0530

    Changed Font Sizes

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 // set up text to print, each item in array is new line
 var aText = new Array(
-    "🙋‍♂️ 21 year old <span class='pink-text'>{</span> <strong>developer</strong><span class='pink-text'> }</span> based in pune", 
+    "🙋‍♂️ 21 year old <span class='pink-text'>{</span> <strong class='dark-text'>developer</strong><span class='pink-text'> }</span> based in pune", 
     "💻 pursuing my bachelor's in computer-science",
     "🎓 @ vishwakarma institute of technology",
-    "💪 currently interning for <span class='pink-text'>{</span> <strong>TomTom</strong><span class='pink-text'> }</span>",
-    "🔥 set to join <span class='pink-text'>{</span> <strong>Veritas</strong><span class='pink-text'> }</span> in summer 2021"
+    "💪 currently interning for <span class='pink-text'>{</span> <strong class='dark-text'>TomTom</strong><span class='pink-text'> }</span>",
+    "🔥 set to join <span class='pink-text'>{</span> <strong class='dark-text'>Veritas</strong><span class='pink-text'> }</span> in summer 2021"
 
     );
-    var iSpeed = 50; // time delay of print out
+    var iSpeed = 20; // time delay of print out
     var iIndex = 0; // start printing array at this posision
     var iArrLength = aText[0].length; // the length of the text array
     var iScrollAt = 20; // start scrolling up at this many lines
